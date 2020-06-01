40bd12341fb2d260c9517dcaaa90bc0dd05766a3
Roberto Barahona <rabarahona1@espe.edu.ec>
2020-06-01 13:06:14 -0500

    Se agregó el CRUD en BEUEjercicio

---
+++ Practica1/BEUEjercicio/Queries/AlumnoQBLL.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BEUEjercicio.Queries
+{
+    public static class AlumnoQBLL
+    {
+        //QBLL Query Bussiness Logic Layer
+        //Capa de Lógica del Negocio - Consultas - Solo lecturas
+        public static List<Alumno> GetAlumnos()
+        {
+            Entities db = new Entities(); //Instancia del contexto
+            return db.Alumno.ToList(); //SQL -> SELECT * From dbo.Alumno
+            //return db.Alumno.Where(x => x.sexo == "M").ToList();
+            //Los métodos del EntityFramework se denomina Linq y la evaluación de condiciones Lambda
+        }
+
+        public static List<Alumno> GetAlumnos(string criterio)
+        {
+            //Ejemplo: criterio = 'vela'
+            //Posibles resultados => Vela, Velasco, Velazquez..., ...vela..
+            Entities db = new Entities();
+            return db.Alumno.Where(x => x.apellidos.ToLower().Contains(criterio)).ToList(); 
+        }
+
+        public static Alumno GetAlumno(int id)
+        {
+            Entities db = new Entities();
+            return db.Alumno.FirstOrDefault(x => x.idalumno == id);
+        }
+
+        public static Alumno GetAlumno(string cedula)
+        {
+            Entities db = new Entities();
+            return db.Alumno.FirstOrDefault(x => x.cedula == cedula);
+        }
+    }
+}
---
+++ Practica1/BEUEjercicio/Transactions/AlumnoTBLL.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BEUEjercicio.Transactions
+{
+    class AlumnoTBLL
+    {
+        //TBLL Query Bussiness Logic Layer
+        //Capa de Lógica del Negocio - Transacciones - Escritura
+
+        private Entities context;
+        public Alumno alumno { get; set } //Alumno activo
+
+        public AlumnoTBLL(bool wiithContext) {
+            if (wiithContext)
+            {
+                context = new Entities();
+            }
+        }
+
+        public void Get(int id)
+        {
+            alumno = context.Alumno.Find(id);
+        }
+
+        public void Create(Alumno a)
+        {
+            using (Entities db = new Entities())
+            {
+                using (var transaction = db.Database.BeginTransaction())
+                {
+                    try
+                    {
+                        db.Alumno.Add(a);
+                        db.SaveChanges();
+                        transaction.Commit();
+                    }
+                    catch (Exception ex)
+                    {
+                        transaction.Rollback();
+                        throw ex;
+                    }
+                }
+            }
+        }
+
+        public void Update()
+        {
+            using (var transaction = context.Database.BeginTransaction())
+            {
+                try
+                {
+                    context.Entry(alumno).State = System.Data.Entity.EntityState.Modified;
+                    context.SaveChanges();
+                    transaction.Commit();
+                }
+                catch (Exception ex)
+                {
+                    transaction.Rollback();
+                    throw;
+                }
+            }
+        }
+
+        public void Delete()
+        {
+            using (var transaction = context.Database.BeginTransaction())
+            {
+                try
+                {
+                    context.Entry(alumno).State = System.Data.Entity.EntityState.Deleted;
+                    context.SaveChanges();
+                    transaction.Commit();
+                }
+                catch (Exception ex)
+                {
+                    transaction.Rollback();
+                    throw;
+                }
+            }
+        }
+
+    }
+}
